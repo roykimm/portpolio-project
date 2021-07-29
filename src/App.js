017e9f63f84c39c1bad57c188cb554aa198439fa
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-07-29 17:48:41 +0900

    header complete!!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar'
+import Header from './components/Header'
+
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-      <Navbar />
+      <>
+        <Particles 
+          className="particles-canvas"
+          params={{
+            number : {
+              value : 30,
+              density : {
+                enable : true,
+                value_area : 900
+              }
+            },
+            shape : {
+              type : "square",
+              stroke: {
+                width : 6,
+                color : "#f9ab00"
+              }
+            }
+          }}
+        />
+        <Navbar />
+        <Header />
+      </>
   );
 }
 
