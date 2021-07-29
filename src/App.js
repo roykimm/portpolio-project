a13fb1141e385b700ecf67be6fd44e12379b2464
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-07-29 20:58:45 +0900

    about me

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import AboutMe from './components/AboutMe';
 
 import Particles from 'react-particles-js';
 
@@ -29,6 +30,7 @@ function App() {
         />
         <Navbar />
         <Header />
+        <AboutMe />
       </>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import author from '../me.jpg';
+
+const AboutMe = () => {
+    return (
+        <div className="container py-5">
+            <div className="row">
+                <div className="col-lg-6 col-xs-12">
+                    <div className="photo-wrap mb-5">
+                        <img className="profile-img" src={author} alt="내사진..." />
+                    </div>
+                </div>
+                <div className="col-lg-6 col-xs-12">
+                    <h1 className="about-heading">About me</h1>
+                    <p>
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                        hello world
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe
